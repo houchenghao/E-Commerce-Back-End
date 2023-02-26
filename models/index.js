e1a040708a639e96e308d4ffe7af46898c4372c1
Chenghao Hou <houchenghao@hotmail.com>
2023-02-26 23:56:12 +1100

    add belongtomany

--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+// Categories have many Products
 Category.hasMany(Product,{
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
@@ -14,18 +15,11 @@ Product.belongsTo(Category,{
   foreignKey: 'category_id',
 }),
 
-// Categories have many Products
-
-
-
-
 // Products belongToMany Tags (through ProductTag)
+// Tags belongToMany Products (through ProductTag)
 
 Product.belongsToMany(Tag,{ through: ProductTag});
 Tag.belongsToMany(Product, {through: ProductTag});
-
-
-// Tags belongToMany Products (through ProductTag)
 
 module.exports = {
   Product,
