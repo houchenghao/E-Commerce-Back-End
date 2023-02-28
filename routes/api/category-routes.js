1ced36c036990158d6e4fe66661f11f8428a59d5
Chenghao Hou <houchenghao@hotmail.com>
2023-02-28 18:20:21 +1100

    added create new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,20 +7,39 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try{
-    const categoriesData = await Category.findAll();
+    const categoriesData = await Category.findAll(
+      {include:[{model: Product}]}
+    );
     res.status(200).json(categoriesData);
   } catch(err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const categoriesData = await Category.findByPk(req.params.id,{
+      include: [{model:Product}]
+    });
+    if(!categoriesData){
+      res.status(404).json({message: 'No category found with this id'});
+    };
+    res.status(200).json(categoriesData);
+  }catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const categoriesData = await Category.create(req.body);
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
