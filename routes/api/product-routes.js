43a5e92ec806bae123273be1cd60ca67ddb93ef0
Chenghao Hou <houchenghao@hotmail.com>
2023-03-01 21:55:55 +1100

    finish tag route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,9 +19,23 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const categoriesData = await Product.findByPk(req.params.id,{
+      include: [
+        {model: Category},
+        {model: Tag}
+      ]
+    });
+    if(!categoriesData){
+      res.status(404).json({message: 'No product found with this id'});
+    };
+    res.status(200).json(categoriesData);
+  }catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -98,8 +112,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json(productData);
+      return;
+    }
+
+    res.status(200).json({message:`delete successfully`});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 module.exports = router;
